3ab47dc9437c813f4f984d80c578a0f859d0918c
Andrew Marek <andwmarek@gmail.com>
2024-10-17 15:44:23 -0700

    inject analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
     import "../app.css";
+    import { dev } from "$app/environment";
+
+    import { inject } from "@vercel/analytics";
     import "$lib/styles/fonts.css";
+
+    inject({ mode: dev ? "development" : "production" });
+
     import Navbar from "$lib/components/navbar.svelte";
 </script>
 
